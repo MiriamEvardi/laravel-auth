8f29c4755b20aa6b26babda9c106a79de7c7e20e
MiriamEvardi <miriamevardi97@gmail.com>
2024-04-23 16:40:13 +0200

    add relation one to many

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,9 @@ class Project extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'description', 'technologies', 'link', 'type_id'];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
